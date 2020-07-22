a793cc2ce71be8831dac942e8e9390368dda6cf5
VocalZero <kacper.mura@web.de>
2020-07-22 20:21:07 +0200

    ✨ New command song

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ const helpCommand = require('./commands/help');
 const listCommand = require('./commands/list');
 const playCommand = require('./commands/play');
 const volumeCommand = require('./commands/volume');
+const songCommand = require('./commands/song');
 client.commands.set('help', helpCommand);
 client.commands.set('list', listCommand);
 client.commands.set('play', playCommand);
 client.commands.set('volume', volumeCommand);
+client.commands.set('song', songCommand);
 
 const ready = require('./events/ready');
 const message = require('./events/message');
--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,3 @@
-
 module.exports = async (client) => {
   console.log(`${client.user.tag}, ready to serve ${client.users.cache.size} users.`);
   console.log(`Loading a total of ${client.commands.size} commands.`);
